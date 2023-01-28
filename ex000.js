a848da90e1be450c09b7c0fe5aa581cf94d5f2b6
Nicolas Victor <nicolas.victor2303@gmail.com>
2023-01-28 00:53:14 -0300

    Exercício 000
    Criar um programa para calcular o valor de uma viagem de carro

---
+++ ex000.js
@@ -0,0 +1,16 @@
+/* 
+    faça um programa para calcular o valor de uma viagem
+    voce tera 3 variaveis
+    1- preco do combustivel
+    2- gasto medio do combustivel do carro por km
+    3- distancia em km da viagem
+*/
+
+const precoCombustivel = 5.79;
+const kmPorLitro = 10.4;
+const distanciaEmKm = 434.2;
+
+const litrosConsumido = distanciaEmKm / kmPorLitro;
+const valorGasto = litrosConsumido * precoCombustivel;
+
+console.log(valorGasto.toFixed(2))
