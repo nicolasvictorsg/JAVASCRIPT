dcf427b07b9a15e53c08d36dc38243912ace038b
Nicolas Victor <nicolas.victor2303@gmail.com>
2023-01-28 05:23:16 -0300

    Update ex000.js

--- ex000.js
+++ ex000.js
@@ -4,6 +4,8 @@
     1- preco do combustivel
     2- gasto medio do combustivel do carro por km
     3- distancia em km da viagem
+
+    imprima no console o valor que sera gasto para realiza a viagem
 */
 
 const precoCombustivel = 5.79;
